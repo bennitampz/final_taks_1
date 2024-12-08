48a3cd070c17f987518f97f8c16ae58ec870f84b
benni-oss <bennitampubolon0@gmail.com>
2024-12-08 11:09:20 +0700

    version one commit

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // POST [name, email, password]
+    public function register(Request $request){
+
+    }
+    // POST [email, password]
+    public function login(Request $request){
+
+    }
+    // GET [Auth:Token]
+    public function profile(Request $request){
+
+    }
+    // GET [Auth:Token]
+    public function logout(Request $request){
+
+    }
+}
